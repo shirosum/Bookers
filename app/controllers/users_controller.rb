a73608eee5c02833ac6ce313166f1eb0372ec6ac
k shiro <kshiro@kenoMacBook-Pro.local>
2019-02-24 17:30:27 +0900

    変更後

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,7 @@ class UsersController < ApplicationController
   def update
   	@user = User.find(params[:id])
   	if @user.update(user_params)
+      flash[:notice] = "Userinfo was successfully updated."
   	  redirect_to user_path(@user.id)
     else
       render :edit
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_065844) do
+ActiveRecord::Schema.define(version: 2019_02_20_062531) do
 
   create_table "books", force: :cascade do |t|
     t.integer "user_id"
@@ -18,7 +18,6 @@ ActiveRecord::Schema.define(version: 2019_02_20_065844) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.text "image_id"
   end
 
   create_table "images", force: :cascade do |t|
